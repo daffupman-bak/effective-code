97003a299886db940bee36f51e2fd46f01e69c5f
daffupman <wzjin85@foxmail.com>
2020-02-24 00:39:05 +0800

    资源自动关闭

--- src/test/java/io/daff/lambda/FileService.java
+++ src/test/java/io/daff/lambda/FileService.java
@@ -17,23 +17,28 @@ public class FileService {
      * @param url 指定的路径
      * @param fileConsumer 自定义的操作
      */
-    public void fileHandle(String url, FileConsumer fileConsumer) throws IOException {
+    public void fileHandle(String url, FileConsumer fileConsumer) {
         // 从本地指定的url处构建文件读取流
-        BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(
-                        new FileInputStream(url)));
+        try (
+            // 需要自动关闭的资源
+            FileInputStream fis = new FileInputStream(url);
+            InputStreamReader reader = new InputStreamReader(fis);
+            final BufferedReader bufferedReader = new BufferedReader(reader)
+        ){
+            // 记录当前行
+            String line;
+            // 记录整个文件的信息
+            StringBuilder builder = new StringBuilder();
+            // 读取文件内容
+            while ((line = bufferedReader.readLine()) != null) {
+                // 如果读取的这行不为空，则将读取的内容放入builder中
+                builder.append(line).append("\n");
+            }
 
-        // 记录当前行
-        String line;
-        // 记录整个文件的信息
-        StringBuilder builder = new StringBuilder();
-        // 读取文件内容
-        while ((line = bufferedReader.readLine()) != null) {
-            // 如果读取的这行不为空，则将读取的内容放入builder中
-            builder.append(line).append("\n");
+            // 调用FileConsumer对象执行自定义的操作
+            fileConsumer.fileHandler(builder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        // 调用FileConsumer对象执行自定义的操作
-        fileConsumer.fileHandler(builder.toString());
     }
 }
